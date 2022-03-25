1a0d8871275b2a6cacffde469af61aef65dd6d15
tobandiramesh <89893609+tobandiramesh@users.noreply.github.com>
2022-03-25 19:28:38 +0530

    Add files via upload
    Added Multi-thread funcationality

--- src/com/datum/purge/tool/DocumentPurgeTool.java
+++ src/com/datum/purge/tool/DocumentPurgeTool.java
@@ -13,6 +13,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
 import com.datum.purge.tool.utils.DocumentPurgeConfigLoader;
+import com.datum.purge.tool.utils.DocumentPurgeExecutor;
 import com.datum.purge.tool.utils.DocumentPurgeLogger;
 import com.datum.purge.tool.utils.DocumentPurgeUtil;
 import com.filenet.api.collection.DocumentSet;
@@ -29,137 +30,64 @@ import com.filenet.api.core.ObjectStore;
  * @version 1.0
  *
  */
-public class DocumentPurgeTool {
+public class DocumentPurgeTool{
 
 	public String className = "DocumentPurgeTool";
+	public static Iterator<Row> rowIterator = null;
+	public static int rowTotalCount = 0 ;
 
-	public void runDocumentPurgeTool() throws Exception {
+	public void runDocumentPurgeTool() {
 
-		ObjectStore objectStore = null;
 		FileInputStream toolInputFile = null;
 		XSSFWorkbook workbook = null;
 		XSSFSheet sheet = null;
-		Iterator<Row> rowIterator = null;
-		StringBuilder searchSQLQuery  = null;
-		FileWriter csvWriter = null;
-		StringBuilder rowStatus = new StringBuilder();
 
 		String methodName = "runDocumentPurgeTool";
-
 
 		try
 		{
 
 			DocumentPurgeLogger.writeLog(className, methodName, DocumentPurgeLogger.DEBUG, "Starting..");
 
-			if(objectStore == null)
-			objectStore = new DocumentPurgeUtil().fetchObjectStore(); 
+			DocumentPurgeConfigLoader objPurgeConfigLoader = new DocumentPurgeConfigLoader();
+			objPurgeConfigLoader.loadDocumentPurgeToolConfigurartion();
+			DocumentPurgeLogger.writeLog(className, methodName, DocumentPurgeLogger.DEBUG, "Tool Configured values are \n"+objPurgeConfigLoader.toString());
 
 			toolInputFile = new FileInputStream(new File(DocumentPurgeConfigLoader.tool_InputFilePath));
 			workbook = new XSSFWorkbook(toolInputFile);
 			sheet = workbook.getSheetAt(0);
-
+			
+			rowTotalCount = sheet.getLastRowNum();
+			
 			rowIterator = sheet.iterator();
 			rowIterator.next();
 
-			while (rowIterator.hasNext()) 
+			int noOfThreads = DocumentPurgeConfigLoader.tool_processThread;
+			
+			for (int count = 0; count < noOfThreads; count++) 
 			{
-				Row row = rowIterator.next();
-				String policyNumber = row.getCell(0).toString();
-				String customerNumber = row.getCell(1).toString();
-
-				searchSQLQuery  = 	new StringBuilder();
-
-				searchSQLQuery.append("Select * from [");
-				searchSQLQuery.append(DocumentPurgeConfigLoader.ce_DocumentClass);
-				searchSQLQuery.append("] where ");
-				searchSQLQuery.append(DocumentPurgeConfigLoader.ce_SearchFields.split(",")[0]+"='");
-				searchSQLQuery.append(policyNumber).append("' AND ");
-				searchSQLQuery.append(DocumentPurgeConfigLoader.ce_SearchFields.split(",")[1]+"=");
-				searchSQLQuery.append(customerNumber);
-
-
-				SearchSQL searchSQL = new SearchSQL(searchSQLQuery.toString());
-				SearchScope search = new SearchScope(objectStore); 
-
-				DocumentSet documentSet = (DocumentSet)search.fetchObjects(searchSQL, 100, null, true);
-
-
-				rowStatus.append(policyNumber);
-				rowStatus.append(",");
-				rowStatus.append(customerNumber);
-				rowStatus.append(",");
-
-
-				if(!documentSet.isEmpty())
-				{
-					Iterator it = documentSet.iterator();
-					int DocCount 	 = 0;
-					while (it.hasNext()) 
-					{
-						Document document = (Document) it.next();
-						document.fetchProperties(new String[]{PropertyNames.ID});
-						
-						DocCount++;
-						
-						String name = document.get_Name();
-						String id = document.get_Id().toString();
-						
-						document.delete();
-						document.save(RefreshMode.REFRESH);
-
-						DocumentPurgeLogger.writeLog(className, methodName, DocumentPurgeLogger.DEBUG, "\n Document Name: " + name +" Document Id: "+id+" has been deleted for Policy Number ["+policyNumber+"] and Customer Number ["+customerNumber+"]");
-
-					}
-
-					DocumentPurgeLogger.writeLog(className, methodName, DocumentPurgeLogger.DEBUG, "\n Total {"+DocCount+"} Document(s) have been deleted for Policy Number ["+policyNumber+"] and Customer Number ["+customerNumber+"]");
-					rowStatus.append(DocCount);
-					rowStatus.append(",");
-					rowStatus.append(" document(s) deleted " );
-
-				}else{
-
-					DocumentPurgeLogger.writeLog(className, methodName, DocumentPurgeLogger.DEBUG, "\n No Document(s) have been found for Policy Number ["+policyNumber+"] and Customer Number ["+customerNumber+"]");
-					rowStatus.append(0);
-					rowStatus.append(",");
-					rowStatus.append(" No documents found " );
-				}
-				rowStatus.append("\n");
-			}
-
-
-			csvWriter = new FileWriter(new Date().getTime()+"_DeletionReport.csv");
-			csvWriter.append("Policy Number");
-			csvWriter.append(",");
-			csvWriter.append("Customer Number");
-			csvWriter.append(",");
-			csvWriter.append("No'of Docs");
-			csvWriter.append(",");
-			csvWriter.append("Status");
-			csvWriter.append("\n"); 
-			csvWriter.append(rowStatus);
-			csvWriter.flush();
-			csvWriter.close();
+				DocumentPurgeExecutor documentPurgeExecutorThread = new DocumentPurgeExecutor();
+				documentPurgeExecutorThread.start();
+			}			
 			toolInputFile.close();
+						
 		} 
 		catch (Exception exception) 
 		{
 			DocumentPurgeLogger.writeErrorLog(className, methodName, "***Exception Occured***", exception); 
 		}finally
 		{
-			objectStore = null;
+			
 			toolInputFile = null;
 			workbook = null;
 			sheet = null;
-			
-			DocumentPurgeLogger.writeLog(className, methodName, DocumentPurgeLogger.DEBUG, "DocumentPurgeTool execution is completed.");
 		}
 	}
-	
-	
+
 	public static void main(String[] args) throws Exception{
 
-		new DocumentPurgeTool().runDocumentPurgeTool();
+		DocumentPurgeTool objDocumentPurgeTool = new DocumentPurgeTool();
+		objDocumentPurgeTool.runDocumentPurgeTool();
 	}
 
 }
